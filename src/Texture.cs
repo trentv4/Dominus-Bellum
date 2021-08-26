e8fc56067df85472690f7f56c2420ed861d232aa
trentv4 <trentvanslyke@gmail.com>
2021-08-26 13:53:17 -0400

    Add ability to load texture from EmbeddedTexture from Assimp

--- src/Texture.cs
+++ src/Texture.cs
@@ -3,6 +3,10 @@ using System.IO;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using StbImageSharp;
+using Assimp;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System;
 
 namespace DominusCore {
 	/// <summary> Class that wraps and provides helper methods for OpenGL textures and framebuffer textures. </summary>
@@ -15,6 +19,7 @@ namespace DominusCore {
 		public readonly int TextureID;
 		/// <summary> Uniform ID assigned via GL.GetUniformLocation() which corresponds to one specific shader uniform. </summary>
 		private readonly int UniformLocation;
+		public static Texture[] MissingTextures;
 
 		/// <summary> Creates a texture using anisotropic filtering, primarily meant for regular texture use. </summary>
 		public Texture(string location, int uniform) {
@@ -32,17 +37,52 @@ namespace DominusCore {
 
 				TextureID = GL.GenTexture();
 				GL.BindTexture(TextureTarget.Texture2D, TextureID);
-				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
-				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)OpenTK.Graphics.OpenGL4.TextureWrapMode.Repeat);
+				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)OpenTK.Graphics.OpenGL4.TextureWrapMode.Repeat);
 				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
 				GL.TexParameter(TextureTarget.Texture2D, (TextureParameterName)All.TextureMaxAnisotropy, anisotropicLevel);
 				GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
-							  image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
+							  image.Width, image.Height, 0, OpenTK.Graphics.OpenGL4.PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
 				GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 
 				LOADED_TEXTURES.Add(location, TextureID);
 			}
 		}
+
+		/// <summary> Creates a texture using anisotropic filtering, primarily meant for regular texture use. </summary>
+		public Texture(string location, int uniform, EmbeddedTexture embedded) {
+			UniformLocation = uniform;
+
+			if (LOADED_TEXTURES.ContainsKey(location)) {
+				this.TextureID = LOADED_TEXTURES[location];
+			} else {
+				float anisotropicLevel = MathHelper.Clamp(16, 1f, MaxAnisotrophy);
+
+				TextureID = GL.GenTexture();
+				GL.BindTexture(TextureTarget.Texture2D, TextureID);
+				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)OpenTK.Graphics.OpenGL4.TextureWrapMode.Repeat);
+				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)OpenTK.Graphics.OpenGL4.TextureWrapMode.Repeat);
+				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+				GL.TexParameter(TextureTarget.Texture2D, (TextureParameterName)All.TextureMaxAnisotropy, anisotropicLevel);
+
+				if (embedded.IsCompressed) {
+					Bitmap bitmap = new Bitmap(new MemoryStream(embedded.CompressedData));
+					BitmapData rawBits = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+														 ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+					GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+								  bitmap.Width, bitmap.Height, 0, OpenTK.Graphics.OpenGL4.PixelFormat.Bgra, PixelType.UnsignedByte, rawBits.Scan0);
+
+					bitmap.UnlockBits(rawBits);
+					bitmap.Dispose();
+				} else {
+					Console.WriteLine("UNCOMPRESSED DATA HELP");
+				}
+
+				GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+				LOADED_TEXTURES.Add(location, TextureID);
+			}
+		}
+
 
 		/// <summary> Creates a Framebuffer texture, NOT MEANT FOR USE ON MODELS. Does not use anisotropic filtering.
 		/// <br/>The colorAttachment parameter is used as an offset from FramebufferAttachment.ColorAttachment0.</summary>
@@ -52,7 +92,7 @@ namespace DominusCore {
 			TextureID = GL.GenTexture();
 			GL.BindTexture(TextureTarget.Texture2D, TextureID);
 			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba16f, Game.WindowSize.X, Game.WindowSize.Y,
-						  0, PixelFormat.Rgba, PixelType.UnsignedByte, new byte[0]);
+						  0, OpenTK.Graphics.OpenGL4.PixelFormat.Rgba, PixelType.UnsignedByte, new byte[0]);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
 			GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMinFilter.Nearest);
 			GL.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0 + colorAttachment,
